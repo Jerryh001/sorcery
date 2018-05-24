e9d8acd4f70598fa898253cb11e660541b368979
aleclarson <alec.stanford.larson@gmail.com>
2018-05-24 19:37:29 -0400

    refactor the `Chain` class into `mergeSourceMaps()`

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-const Node = require("./Node.js");
-const Chain = require("./Chain.js");
+const { dirname, relative, resolve } = require("path");
+const { encode } = require("sourcemap-codec");
+const SourceMap = require("./SourceMap.js");
 const assert = require("invariant");
-
-function noop() {
-  return null;
-}
+const Node = require("./Node.js");
 
 function sorcery(sources, opts = {}) {
   const len = sources.length;
@@ -41,11 +39,92 @@ function sorcery(sources, opts = {}) {
     nodes[i] = node;
   }
 
-  // The last source may have a sourcemap.
+  // The last source given might be generated.
   nodes[len - 1].loadSources(opts);
 
-  // Trace back to the last source(s).
-  return (new Chain(nodes[0])).apply(opts);
+  // Trace back to the original source(s).
+  return mergeSourceMaps(nodes[0], opts);
 }
 
 module.exports = sorcery;
+
+function mergeSourceMaps(node, opts) {
+  const names = [];
+  const sources = [];
+  const mappings = resolveMappings(node, names, sources);
+
+  const generatedFile = opts.generatedFile || node.file;
+  const sourceRoot = opts.sourceRoot
+    ? resolve(opts.sourceRoot)
+    : generatedFile
+      ? dirname(generatedFile)
+      : process.cwd();
+
+  const includeContent = opts.includeContent !== false;
+  const sourcesContent = sources.map(
+    includeContent ? opts.readFile : () => null
+  );
+
+  return new SourceMap({
+    file: generatedFile ? relative(sourceRoot, generatedFile) : null,
+    sources: sources.map(source => slash(relative(sourceRoot, source))),
+    sourceRoot: slash(sourceRoot),
+    sourcesContent,
+    names,
+    mappings
+  });
+}
+
+function resolveMappings(node, names, sources) {
+  let i = node.mappings.length;
+  let mappings = new Array(i);
+  while (i--) {
+    let resolved = [], len = 0;
+    mappings[i] = resolved;
+
+    const line = node.mappings[i];
+    for (let j = 0; j < line.length; j++) {
+      let segment = line[j];
+      if (segment.length >= 4) {
+        const traced = node.sources[segment[1]].trace(
+          segment[2], // source code line
+          segment[3], // source code column
+          node.map.names[segment[4]]
+        );
+        if (traced) {
+          let sourceIndex = sources.indexOf(traced.source);
+          if (sourceIndex == -1) {
+            sourceIndex = sources.length;
+            sources.push(traced.source);
+          }
+
+          // the resolved segment
+          resolved[len++] = segment = [
+            segment[0], // generated code column
+            sourceIndex,
+            traced.line - 1,
+            traced.column
+          ];
+
+          if (traced.name) {
+            let nameIndex = names.indexOf(traced.name);
+            if (nameIndex == -1) {
+              nameIndex = names.length;
+              names.push(traced.name);
+            }
+            segment[4] = nameIndex;
+          }
+        }
+      }
+    }
+  }
+  return encode(mappings);
+}
+
+function slash(path) {
+  return typeof path === "string" ? path.replace(/\\/g, "/") : path;
+}
+
+function noop() {
+  return null;
+}
